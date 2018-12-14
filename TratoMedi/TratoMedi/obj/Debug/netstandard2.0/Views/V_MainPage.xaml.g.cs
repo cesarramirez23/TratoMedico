b23934d1d7d3f4cfc33618ef57989ceaad4b77b3
Saul Lopez <saullopez@MacBook-Pro-de-Saul.local>
2018-12-14 15:51:35 -0600

    io

--- TratoMedi/TratoMedi/obj/Debug/netstandard2.0/Views/V_MainPage.xaml.g.cs
+++ TratoMedi/TratoMedi/obj/Debug/netstandard2.0/Views/V_MainPage.xaml.g.cs
@@ -13,7 +13,7 @@
 namespace TratoMedi.Views {
     
     
-    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("Views\\V_MainPage.xaml")]
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("Views/V_MainPage.xaml")]
     public partial class V_MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
